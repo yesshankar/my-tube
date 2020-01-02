defda15d2b21de7ead8a9ed5a6d62d22d3b7e288
Shankar Khatri <yes143.shankar@gmail.com>
2020-01-02 00:40:17 -0600

    Updates

---
+++ precache-manifest.5d8cd0a7746fd66067573b3dd652a4e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae8f3b9c001ceadd9436ecc51fdcd87",
+    "url": "/my-tube/index.html"
+  },
+  {
+    "revision": "6eeb24281ab3a071ce22",
+    "url": "/my-tube/static/css/main.eeb7d9f4.chunk.css"
+  },
+  {
+    "revision": "541000348b17e0da1ad1",
+    "url": "/my-tube/static/js/2.37b94e26.chunk.js"
+  },
+  {
+    "revision": "e92e356b467ce5268290e03394dbf217",
+    "url": "/my-tube/static/js/2.37b94e26.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6eeb24281ab3a071ce22",
+    "url": "/my-tube/static/js/main.0043694c.chunk.js"
+  },
+  {
+    "revision": "6fc177424f1476e40b55",
+    "url": "/my-tube/static/js/runtime-main.6171efb3.js"
+  }
+]);
